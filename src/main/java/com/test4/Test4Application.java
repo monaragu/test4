36b92240da9950ad2812b71f288c8eaa09be270f
monaragu <monalisaragu@gmail.com>
2025-05-08 22:20:29 +0530

    Update Test4Application.java

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Test4Application {
 
+	int x=100;
+	System.out.println("Hello World");
 	public static void main(String[] args) {
 		SpringApplication.run(Test4Application.class, args);
 	}
